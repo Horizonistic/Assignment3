5e539a2bc0ae23546e6859775b624f917ff89d37
Jacob <Horizonistic@gmail.com>
2016-02-25 16:01:24 -0800

    Char array testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <fstream>
 #include <stdlib.h>
 #include <iostream>
@@ -5,30 +6,48 @@
 
 using namespace std;
 
+const int ISBN_MAX_LENGTH = 13;
 const int EXAMPLE = -1;
 const int EXIT_PROGRAM = -2;
 
-string input(SuperOutput * so)
+char * input(SuperOutput * so)
 {
+    while (true)
+    {
+        char input[ISBN_MAX_LENGTH];
 
+        so->println("Please enter a valid ISBN: ");
+        cin.getline(input, ISBN_MAX_LENGTH) >> input;
+        //if (input == EXAMPLE)
+        {
+
+        }
+    }
 }
 
 void example(SuperOutput * so)
 {
-
+    // TODO
 }
 
 bool checkDigits(char * isbn)
 {
-
+    // TODO
 }
 
 bool checkSum(char * isbn)
 {
-
+    // TODO
 }
 
 int main()
 {
     SuperOutput * so = new SuperOutput("output.txt");
+    char name[3];
+    cin.getline(name, 4);
+    for (int i = 0; i < sizeof(name); i++)
+    {
+        char s = (char) name[i];
+        so->println(s);
+    }
 }
